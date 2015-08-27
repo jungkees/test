70c6aec0599a2c8d841b79cc7204ea7161d5d66d
Jungkee Song <jungkee.song@samsung.com>
2015-08-27 14:39:01 +0900

    Add fetch.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.onmessage = function(e) {
+self.addEventListener("message", function(e) {
   if (e.data == 'page-got-controller') {
     clients.matchAll().then(function(clients) {
       clients.forEach(function(c) {
@@ -7,3 +7,19 @@ self.onmessage = function(e) {
     });
   }
 };
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("shell-v1").then(function(cache) {
+      return cache.add("/index.html");
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
